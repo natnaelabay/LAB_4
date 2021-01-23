625f66a2e310e39c4e9aa2ff401d65045f3a50ed
Natnael Abay <se.natnael.abay@gmail.com>
2021-01-23 23:55:51 +0300

    downgrade: removed localstorage and used objects

--- bank/assets/js/bank.js
+++ bank/assets/js/bank.js
@@ -1,84 +1,129 @@
 
+let accounts = [
+    { name: "Natnael Abay", acc_id: 1, amount: 4_000 },
+    { name: "Kebede Chala", acc_id: 2, amount: 2_000 },
+
+]
+
+let findAccount = (id, accounts) => {
+    for (let i = 0; i < accounts.length; i++) {
+        if (accounts[i].acc_id == id) {
+            return { account: accounts[i], isTrue: 1 }
+        }
+    }
+    return { isTrue: 0 }
+}
+
 let deposit = (id, amount) => {
-    let account = localStorage.getItem(id)
-    if (account == null) {
-        localStorage.setItem(id,amount)
-    } else {
-        localStorage.setItem(id,parseInt(amount) + parseInt(account)) 
+    let account = findAccount(id, accounts).accounts
+    if (account.isTrue == 0)
+        console.log("there is no account with the specified account number!")
+    else {
+        account.amount = parseInt(account.amount) + parseInt(amount)
+        console.log(`you have successfully deposited ${amount} your current account balance is ${account.amount}`)
     }
 }
+
 let withdraw = (id, amount) => {
-    let account = localStorage.getItem(id)
-    if (account == null) {
-        console.log("no user");
+    let account = findAccount(id, accounts).account
+    if (account.isTrue == 0) {
+        console.log(`There is no Account with ${id} account number try again!!!`);
     } else {
-        if (account < amount) {
-            console.log("amount not enough");
-        } else if(amount > 1000) {
-            console.log("max amount reached ");
-        }else {
-            localStorage.setItem(id, parseInt(account) - parseInt(amount))
-            console.log("successfull withdrawn " + amount);
+        if (account.amount < amount) {
+            console.log(`Your current account balance ${account.amount} is insufficient to withdraw the amount ------ ${amount} ------ 😥😥 `);
+        } else if (amount > 1000) {
+            console.log(`maximum withdraw limit please decrease the limit of money <= 100`);
+        } else {
+            account.amount = parseInt(account.amount) - parseInt(amount)
+            console.log(`successfully withdrawn ------ ${amount} ETB ------ your current account balance is ---- ${account.amount} ----- 😊`);
         }
     }
 }
 let balance = (id) => {
-    let account = localStorage.getItem(id)
-    if (account == null) {
-        console.log("no user");
-    } else {
-        console.log("account balance is " + account);
-    }
+    let account = findAccount(id, accounts).account
+    if (account.isTrue == 0)
+        console.log(`There is no Account with ${id} account number try again!!!`);
+    else
+        console.log(`Dear ${account.name} with account number ${account.acc_id} your account balance is ----- ${account.amount} ETB ----- thank you for your  service`)
 }
 
 let transfer = (id, to, amount) => {
-    let account = localStorage.getItem(id)
-    let for_who = localStorage.getItem(to)
-    if (account < amount) {
-        console.log("not enough money!");
+    let account = findAccount(id, accounts).account
+    let for_who = findAccount(to, accounts).account
+    if (account.isTrue == 0 ) {
+        console.log(`Dear customer the account number you inserted is not correct!!`);
+        
     } else {
-        if (to == null) {
-            console.log("no user with that id");
-        } else {
-            localStorage.setItem(id, parseInt(account) - parseInt(amount))
-            localStorage.setItem(to, parseInt(for_who) + parseInt(amount))
-            console.log("transfered birr");
+        if (account.amount < amount)
+            console.log(`Your current account balance ------- ${account.amount} ETB ------- is insufficient to make ----- ${amount} ETB ----- transfer!!`);
+        else {
+            if (for_who.isTrue == 0)
+                console.log(`Dear customer you have inserted an incorrect account number for your transfer please try again!!`);
+            else {
+                account.amount = parseInt(account.amount) - parseInt(amount)
+                for_who.amount = parseInt(for_who.amount) + parseInt(amount)
+                console.log(`Dear ${account.name} you have successfully transferred ------ ${amount} ETB ------  to ${for_who.name} 😊`)
+            }
         }
     }
 }
 
+let register = (acc_id, amount, name) => {
+    acc_id = parseInt(acc_id)
+    accounts.push({
+        name,
+        amount,
+        acc_id
+    })
+}
 
-let register = (id, amount) => {
-    localStorage.setItem(id, amount)
+let print = (accounts) => {
+    console.log("\n")
+    console.table(accounts)
+    console.log("\n")
 }
 
 
 (function () {
-    let choice = prompt("register: 1 transfer:2 balance:3 withdraw:4 deposit: 5")
-    if (choice == "1") {
-        let id = prompt("acc no.")
-        let amount = prompt("ammount")
-        register(id, parseInt(amount))
-    }
-    else if (choice == "2") {
-        let id = prompt("acc no.")
-        let to = prompt("to acc")
-        let amount = prompt("ammount")
-        transfer(id, to, parseInt(amount))
-    }
-    else if (choice == "3") {
-        let id = prompt("acc no.")
-        balance(id)
-    } else if (choice == "4") {
-        let id = prompt("acc no.")
-        let amount = prompt("ammount")
+    print(accounts)
+    let choice = prompt("register: 1 \n transfer: 2 \n balance: 3 \n withdraw:4 \n deposit: 5 \n anyKeyToQuit:😞")
+    while (true) {
 
-        withdraw(id,amount)
-    }
-    else if (choice == "5") {
-        let id = prompt("acc no.")
-        let amount = prompt("ammount")
+        if (choice == "1") {
+            let id = prompt("acc no.")
+            let amount = prompt("amount")
+            let fullName = prompt("your full name?")
+            register(id, parseInt(amount), fullName)
+            print(accounts)
+        }
+        else if (choice == "2") {
+            let id = prompt("acc no.")
+            let to = prompt("to acc")
+            let amount = prompt("amount")
+            transfer(id, to, parseInt(amount))
+            print(accounts)
+        }
+        else if (choice == "3") {
+            let id = prompt("acc no.")
+            balance(id)
+            print(accounts)
+        } else if (choice == "4") {
+            let id = prompt("acc no.")
+            let amount = prompt("amount")
+            withdraw(id, amount)
+            print(accounts)
+        }
+        else if (choice == "5") {
+            let id = prompt("acc no.")
+            let amount = prompt("amount")
+            deposit(id, amount)
+            print(accounts)
+        } else {
+            console.log()
+            break
 
-        deposit(id,amount)
+        }
+
+        choice = prompt("register: 1 \n transfer: 2 \n balance: 3 \n withdraw:4 \n deposit: 5 \n anyKeyToQuit:😞")
     }
 })()
